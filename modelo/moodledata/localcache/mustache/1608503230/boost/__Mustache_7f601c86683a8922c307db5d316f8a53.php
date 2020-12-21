51505258431f0af16b743db4875e6076006d2be2
Afelipe1599 <andres.montes@correounivalle.edu.co>
2020-12-21 01:21:29 -0500

    backup del moodle

---
+++ modelo/moodledata/localcache/mustache/1608503230/boost/__Mustache_7f601c86683a8922c307db5d316f8a53.php
@@ -0,0 +1,15 @@
+<?php
+
+class __Mustache_7f601c86683a8922c307db5d316f8a53 extends Mustache_Template
+{
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $buffer = '';
+
+        if ($partial = $this->mustache->loadPartial('core_admin/setting_configfile')) {
+            $buffer .= $partial->renderInternal($context);
+        }
+
+        return $buffer;
+    }
+}
